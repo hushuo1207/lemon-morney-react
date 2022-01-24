7fa6472d6a0bf8354472ff5bb20c940561020bce
hushuo <1253740807@qq.com>
2022-01-24 22:22:02 +0800

    安装styled-component CSS-in-JS

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import './yyy/bbb.tsx'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +11,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
