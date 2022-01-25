eb36f5f48ed0322314cf12ba64154b0107fc6bd9
hushuo <1253740807@qq.com>
2022-01-25 21:09:11 +0800

    Money.tsx 之 TagsSection UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -3,17 +3,40 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TagSection = styled.section`
-  
-`
+  background: #FFFFFF;
+  padding: 12px 16px;
+
+  > ol {
+    margin: 0 -12px;
+
+    > li {
+      background: #D9D9D9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
+    }
+  }
+
+  > button {
+    background: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
+    margin-top: 8px;
+  }
+`;
 const NotesSection = styled.section`
-  
-`
+
+`;
 const CategorySection = styled.section`
-  
-`
+
+`;
 const NumberPadSection = styled.section`
-  
-`
+
+`;
 
 function Money() {
   return (
