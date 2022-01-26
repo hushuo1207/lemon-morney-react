9af900a1f50262295861f8a419c79d588e185368
hushuo <1253740807@qq.com>
2022-01-26 22:34:37 +0800

    完成标签的选择和新增

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -16,15 +16,7 @@ const MyLayout = styled(Layout)`
 function Money() {
   return (
     <MyLayout>
-      <TagSection>
-        <ol>
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
-        </ol>
-        <button>新增标签</button>
-      </TagSection>
+      <TagSection />
       <NotesSection>
         <label>
           <span>notes:</span>
--- src/views/Money/TagSection.tsx
+++ src/views/Money/TagSection.tsx
@@ -1,32 +1,58 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const TagSection = styled.section`
-  background: #FFFFFF;
-  padding: 12px 16px;
-  display: flex;
-  flex-grow: 1;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items:flex-start;
-  > ol {
-    margin: 0 -12px;
-    > li {
-      background: #D9D9D9;
-      border-radius: 18px;
-      display: inline-block;
-      padding: 3px 18px;
-      font-size: 14px;
-      margin: 8px 12px;
+const Wrapper = styled.section`
+  background: #FFFFFF; padding: 12px 16px;
+  flex-grow: 1; display:flex; flex-direction: column;
+  justify-content: flex-end; align-items: flex-start;
+  > ol { margin: 0 -12px;
+    > li{
+      background: #D9D9D9; border-radius: 18px;
+      display:inline-block; padding: 3px 18px;
+      font-size: 14px; margin: 8px 12px;
+      &.selected{
+        background: #f60;
+      }
     }
   }
-  > button {
-    background: none;
-    border: none;
-    padding: 2px 4px;
-    border-bottom: 1px solid #333;
-    color: #666;
+  > button{
+    background:none; border: none; padding: 2px 4px;
+    border-bottom: 1px solid #333; color: #666;
     margin-top: 8px;
   }
 `;
+const TagSection: React.FC = () => {
+  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const onAddTag = () => {
+    const tagName = window.prompt('新标签的名称为');
+    if (tagName !== null) {
+      setTags([...tags, tagName]);
+    }
+  };
+  const onToggleTag = (tag: string) => {
+    const index = selectedTags.indexOf(tag);
+    if (index >= 0) {
+      // 如果 tag 已被选中，就复制所有没有被选中的 tag，作为新的 selectedTag
+      setSelectedTags(selectedTags.filter(t => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+  const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map(tag =>
+          <li key={tag} onClick={
+            () => {onToggleTag(tag);}
+          } className={getClass(tag)}
+          >{tag}</li>
+        )}
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>
+  );
+};
 
 export {TagSection};
