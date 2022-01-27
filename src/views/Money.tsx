de6a729d14fd6ef5acb131b17e8f422e43002c2d
hushuo <1253740807@qq.com>
2022-01-27 22:09:25 +0800

    重构：将 tag:string 重构为 tag:{id:number;name:string}

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -13,27 +13,21 @@ const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;
 `;
+
 function Money() {
   const [selected, setSelected] = useState({
-  tags: [] as string[],
-  note: '',
-  category: '-' as Category,
-  amount: 0
+    tagIds: [] as number[],
+    note: '',
+    category: '-' as Category,
+    amount: 0
   });
   const onChange = (obj: Partial<typeof selected>) => {
     setSelected({...selected, ...obj});
   };
   return (
     <MyLayout>
-      {selected.tags.join(',')}
-      <hr/>
-      {selected.note}
-      <hr/>
-      {selected.category}
-      <hr/>
-      {selected.amount}
-      <TagsSection value={selected.tags}
-                   onChange={tags => onChange({tags})}/>
+      <TagsSection value={selected.tagIds}
+                   onChange={tagIds => onChange({tagIds})}/>
       <NoteSection value={selected.note}
                    onChange={note => onChange({note})}/>
       <CategorySection value={selected.category}
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,5 +1,5 @@
 import Layout from '../components/Layout';
-import React, {useState} from 'react';
+import React from 'react';
 import {useTags} from 'useTags';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
@@ -37,14 +37,14 @@ const Space = styled.div`
 `
 
 function Tags() {
-  const {tags, setTags} = useTags();
+  const {tags} = useTags();
   return (
     <Layout>
       <TagList>
         {tags.map(tag =>
-          <li key={tag}>
+          <li key={tag.id}>
             <Link to={'/tags/' + tag}>
-              <span className="oneLine">{tag}</span>
+              <span className="oneLine">{tag.name}</span>
               <Icon name="right"/>
             </Link>
           </li>
