0334aaf6f4de4826d92d6db3e29ba01a080d7127
hushuo <1253740807@qq.com>
2022-01-27 20:25:18 +0800

    category 切换功能搞定

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -18,12 +18,7 @@ function Money() {
     <MyLayout>
       <TagSection/>
       <NoteSection/>
-      <CategorySection>
-        <ul>
-          <li className="selected">支出</li>
-          <li>收入</li>
-        </ul>
-      </CategorySection>
+      <CategorySection/>
       <NumberPadSection>
         <div className="output">
           100
