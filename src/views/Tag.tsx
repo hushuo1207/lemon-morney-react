7430a4a6048b30a5dcf5f7c11013ac64aa45609d
hushuo <1253740807@qq.com>
2022-02-07 16:04:17 +0800

    创建单独的 Button 组件

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import {useTags} from '../useTags';
 import {useParams} from 'react-router-dom';
+import Layout from '../components/Layout';
+import {Button} from '../components/Button';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
 
 type Params = {
   id: string
 }
+const Topbar = styled.header`
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+  background:white;
+`;
 const Tag: React.FC = (propos) => {
   const {findTag} = useTags();
   let {id} = useParams<Params>();
   const tag = findTag(parseInt(id));
   return (
-    <div>{tag.name}</div>
+    <Layout>
+      <Topbar>
+        <Icon name="left"/>
+        <span>编辑标签</span>
+        <Icon/>
+      </Topbar>
+      <div>
+        <label>
+          <span>标签名</span>
+          <input type="text" placeholder="标签名"/>
+        </label>
+      </div>
+      <div>
+        <Button>删除标签</Button>
+      </div>
+    </Layout>
   );
 };
 
