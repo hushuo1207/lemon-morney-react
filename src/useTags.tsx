24669c856a7641b27f0feaae6e4468a37b52f99d
hushuo <1253740807@qq.com>
2022-02-07 16:51:56 +0800

    编辑 tag 名

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -11,7 +11,28 @@ const defaultTags = [
 const useTags = () => { // 封装一个自定义 Hook
   const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
   const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
-  return {tags, setTags, findTag};
+
+  const findTagIndex = (id: number) => {
+    let result = -1;
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[i].id === id) {
+        result = i;
+        break;
+      }
+    }
+    return result;
+  };
+  const updateTag = (id: number, obj: { name: string }) => {
+    // 获取你要改的 tag 的下标
+    const index = findTagIndex(id);
+    // 深拷贝 tags 得到 tagsClone
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    // 把 tagsClone 的第 index 删掉，换成 {id:id, name: obj.name}
+    tagsClone.splice(index, 1, {id: id, name: obj.name});
+    console.log(tagsClone);
+    setTags(tagsClone);
+  };
+  return {tags, setTags, findTag, updateTag, findTagIndex};
 };
 
 export {useTags};
