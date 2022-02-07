88b067783ca2ef3795e676db3a7bfde38ef7333a
hushuo <1253740807@qq.com>
2022-02-07 17:28:47 +0800

    点击回退

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import cs from 'classnames';
 // require('icons/money.svg')
 // require('icons/tag.svg')
 // require('icons/statistics.svg')
@@ -9,12 +9,13 @@ try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {consol
 
 type Props = {
   name?: string;
-}
+} & React.SVGAttributes<SVGElement>
 
 
 const Icon = (props: Props) => {
+  const {name, children, className, ...rest} = props
   return (
-    <svg className="icon">
+    <svg className={cs('icon', className)} {...rest}>
       {props.name && <use xlinkHref={'#' + props.name}/>}
     </svg>
   )
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useTags} from '../useTags';
-import {useParams} from 'react-router-dom';
+import {useParams, useHistory} from 'react-router-dom';
 import Layout from '../components/Layout';
 import {Button} from '../components/Button';
 import styled from 'styled-components';
@@ -49,11 +49,14 @@ const Tag: React.FC = () => {
       </Center>
     </div>
   );
-
+  const history = useHistory()
+  const onClickBack = ()=>{
+    history.goBack()
+  }
   return (
     <Layout>
       <Topbar>
-        <Icon name="left"/>
+        <Icon name="left" onClick={onClickBack}/>
         <span>编辑标签</span>
         <Icon/>
       </Topbar>
