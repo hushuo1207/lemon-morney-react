f8f556a491d77feb1a73f02189094f0100f8fb53
hushuo <1253740807@qq.com>
2022-02-10 17:01:25 +0800

    404 页面优化

--- src/views/NoMatch.tsx
+++ src/views/NoMatch.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import {Center} from 'components/Center'
+import { Space } from 'components/Space';
+import styled from 'styled-components';
+import { Button } from 'components/Button';
+import {NavLink} from 'react-router-dom';
+
+const Content = styled.div`
+  font-size: 48px;
+`
 
 function NoMatch() {
-  return <div>404页面不存在，请输入正确地址</div>;
+
+  return (
+    <Center>
+      <Space/>
+      <Space/>
+      <Space/>
+      <Content>
+        404
+      </Content>
+      <Space/>
+      <Space/>
+      <Space/>
+      页面不存在，请点击下面按钮
+      <Space/>
+      <Space/>
+      <Space/>
+      <Button>
+        <NavLink to="/money">
+          返回首页
+        </NavLink>
+      </Button>
+    </Center>
+  );
 }
 
 
